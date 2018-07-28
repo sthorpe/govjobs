ecdf1f95b4ad161315f9c01cfe76d1885581c760
Luis Diaz <diaz.luispr@gmail.com>
2018-07-27 21:00:55 -0400

    Little Test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import FormContainer from "./js/components/container/FormContainer";
+import Input from "./js/components/presentational/Input";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<Input/>, document.getElementById("create-article-form"));
+
